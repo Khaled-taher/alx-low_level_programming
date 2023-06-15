6a63639be818e53852499383385dcbd8b82005b2
Khaled-taher <khaledtaher98@gmail.com>
2023-06-15 22:20:25 +0300

    advanced tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * mani - Entery point
- *
+ * main - Entery point
  * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+stdout);
 	return (1);
 }
