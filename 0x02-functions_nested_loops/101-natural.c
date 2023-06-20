6268a1d66ebd122159f9676d9dec14ab1c1d0bab
Khaled-taher <khaledtaher98@gmail.com>
2023-06-21 00:04:35 +0300

    advanced tasks

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,22 +4,26 @@
 
 /* betty style doc for function main goes there */
 /**
+ * main - Point of start
  * print_natural - Print the alphabet 10 tims
- *
- * Return: return nothing
+ * Return: return 0
  */
 int main(void)
 {
 	print_natural();
 	return (0);
 }
-
+/* betty style doc for function print_natural goes there */
+/**
+ * print_natural - add multiples of 3 and 5 together
+ *
+ * return: Return nothing
+ */
 void print_natural(void)
 {
 	int i, sum;
-	
+
 	sum = 0;
-	
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
