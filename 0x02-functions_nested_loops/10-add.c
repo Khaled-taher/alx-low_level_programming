eb6edbad0e6498f93ee29a24d4412202867e61bd
Khaled-taher <khaledtaher98@gmail.com>
2023-06-20 21:19:39 +0300

    mandatory tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/* more headers goes there */
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * add - add 2 numbers together
+ * @a: First number
+ * @b: Second number
+ * Return: sum of 2 numbers
+ */
+int add(int a, int b);
+{
+	return (a + b);
+}
