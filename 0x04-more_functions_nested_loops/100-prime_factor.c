3d9e5bb08f627388087cdd1fa4ec9bd5d4c0814d
Khaled-taher <khaledtaher98@gmail.com>
2023-06-22 19:57:32 +0300

    advanced tasks

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int n, i, prime;
+	double nx;
+
+	n = 612852475143;
+	nx = sqrt(n);
+
+	for (i = 1; i <= nx; i++)
+	{
+		if (n % i == 0)
+			prime = i;
+	}
+	printf("%ld\n", prime);
+	return (0);
+}
