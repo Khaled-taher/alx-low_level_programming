c79dfc522b81237335b462d9d0d7b427cf6d5206
Khaled-taher <khaledtaher98@gmail.com>
2023-06-29 20:35:39 +0300

    mandatory tasks

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,8 +11,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int k;
 
-	for (k = 0; k < n; k++)
+	for (k = 0; k < n && src[k] != '\0'; k++)
 		dest[k] = src[k];
+
+	while (k < n)
+	{
+		dest[k] = '\0';
+		i = i + 1;
+	}
 
 	return (dest);
 
