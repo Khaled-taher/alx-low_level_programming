333c290c21bc9b9c935aa43277484e67b5388c44
Khaled-taher <khaledtaher98@gmail.com>
2023-07-03 21:54:49 +0300

    advanced tasks

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - concatinate to strings
+ * @s: pointer to area of memory
+ * @to: constant byte
+ * Return: pointer to s
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
