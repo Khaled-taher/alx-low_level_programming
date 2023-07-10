78b66a348f03544464a9299f0b627a0017799fc1
Khaled-taher <khaledtaher98@gmail.com>
2023-07-10 21:17:10 +0300

    mandatory tasks

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **arr;
 
-	if (width == 0 || height == 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	arr = malloc(sizeof(int *) * height);
@@ -25,17 +25,16 @@ int **alloc_grid(int width, int height)
 
 		if (arr[i] == NULL)
 		{
-			for (j = 0; j <= i; j++)
-				free(arr[i]);
+			for (j = 0; j < i; j++)
+				free(arr[j]);
 			
 			free(arr);
 			return (NULL);
 		}
-	}
 
-	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
+	}
 
 	return (arr);
 }
