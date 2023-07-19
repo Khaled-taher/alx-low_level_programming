91be02f00b10c10f42ce374ac47a204bd855581f
Khaled-taher <khaledtaher98@gmail.com>
2023-07-19 19:29:17 +0300

    all tasks

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,16 +1,20 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
- * array_iterator - concatinate to strings
- * @array: destination to concatinate after
- * @size: scource taht will be concatinated
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array
+ *
+ * @array: array
+ * @size: size of array
  * @action: pointer to function
- * Return: pointer to destination
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	if (!array || !action)
+		return;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	while (size--)
+		action(*array++);
 }
