b29ed777d3b3099d738eeddd5e0d0a97381a07b5
Khaled-taher <khaledtaher98@gmail.com>
2023-07-19 19:31:22 +0300

    all tasks

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,24 +1,25 @@
 #include "function_pointers.h"
 
 /**
- * int_index - concatinate to strings
- * @array: destination to concatinate after
- * @size: scource taht will be concatinated
- * @cmp: pointer to function
- * Return: pointer to destination
+ * int_index - searches for an integer.
+ * @array: array to search in
+ * @size: size of the array
+ * @cmp: pointer to the comparing function
+ * Return: index of the first element for which the cmp
+ * function does not return 0, or -1 if no match is found
+ * or size is negative
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
 	}
-
 	return (-1);
 }
