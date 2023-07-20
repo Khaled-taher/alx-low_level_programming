4c711e2040ef6092941f831be9f6ca4d427e61b5
Khaled-taher <khaledtaher98@gmail.com>
2023-07-20 22:18:10 +0300

    mandatory tasks

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,36 +1,23 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings - concatinate to strings
- * @separator: the char used to separate numbers
- * @n: scource taht will be concatinated
- * Return: pointer to destination
+ * print_strings - prints strings, followed by a new line
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to the function
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	char *s;
+	char *str;
 	va_list args;
 
 	va_start(args, n);
-
 	for (i = 0; i < n; i++)
 	{
-		s = va_arg(args, char *);
-
-		if (s == NULL)
-			printf("nil");
-		else
-			printf("%s", s);
-
-		if (i == n - 1)
-			continue;
-
-		if (separator == NULL)
-			putchar(' ');
-		else
-			printf("%s", separator);
+		str = va_arg(args, char*);
+		printf("%s%s", str == NULL ? "(nil)" : str,
+			   (i < n - 1 && separator != NULL) ? separator : "");
 	}
-	putchar('\n');
 	va_end(args);
+	printf("\n");
 }
