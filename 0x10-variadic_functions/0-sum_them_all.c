2cb1b4a8cb2bac192d028b9fe81814a15e383a8d
Khaled-taher <khaledtaher98@gmail.com>
2023-07-20 22:12:17 +0300

    mandatory tasks

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,27 +1,23 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - concatinate to strings
- * @n: scource taht will be concatinated
- * Return: pointer to destination
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of parameters
+ * Return: sum of all parameters
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list ap;
 	unsigned int i;
-	int x, s = 0;
-	va_list args;
+	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	va_start(args, n);
-
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
-	{
-		x = va_arg(args, int);
-		s = s + x;
-	}
-	va_end(args);
+		sum += va_arg(ap, int);
+	va_end(ap);
 
-	return (s);
+	return (sum);
 }
