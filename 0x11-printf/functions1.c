fc9eb0f92485c8408b79fe93e45ddaf7d6f7bb6e
Khaled-taher <khaledtaher98@gmail.com>
2023-07-23 20:23:27 +0300

    mandatory tasks

--- 0x11-printf/functions1.c
+++ 0x11-printf/functions1.c
@@ -8,7 +8,16 @@
 int print_int(va_list args)
 {
 	int n = va_arg(args, int);
-	int i, temp = n, exp = 1, dig = 0;
+	int i, temp, exp = 1, dig = 0, sig = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		sig = 1;
+		n = n * -1;
+	}
+
+	temp = n;
 
 	while (temp != 0)
 	{
@@ -23,10 +32,10 @@ int print_int(va_list args)
 	{
 		exp = exp / 10;
 		_putchar((temp / exp) + '0');
-		temp = temp % 10;
+		temp = temp % exp;
 	}
 
-	return (dig);
+	return (dig + sig);
 }
 
 /**
@@ -58,3 +67,31 @@ int print_bin(va_list args)
 	return (bt);
 }
 
+/**
+ * print_unsig - print number in unsigned form
+ * @args: number to be printed in unsigned form
+ * Return: number of printed digit
+ */
+int print_unsig(va_list args)
+{
+	int n = va_arg(args, int);
+	int i, temp = n, exp = 1, bt = 0;
+
+	while (temp != 0)
+	{
+		temp = temp / 2;
+		exp = exp * 2;
+		bt++;
+	}
+
+	temp = n;
+
+	for (i = 0; i < bt; i++)
+	{
+		exp = exp / 2;
+		_putchar((temp / exp) + '0');
+		temp = temp % exp;
+	}
+
+	return (bt);
+}
--- 0x11-printf/main.c
+++ 0x11-printf/main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	_printf("");
-	_printf("this is %s first try, this is a first letter of my name %c, my garde is %d.%i%%, in binary %b and %b\n", "my", 'K', 148, 56, 16, 7);
+	_printf("this is %s first try, this is a first letter of my name %c, my garde is %d in minus form %i%%, in binary %b and %b\n", "my", 'K', 148, -148, 16, 7);
 	_printf("%");
 	return (0);
 }
