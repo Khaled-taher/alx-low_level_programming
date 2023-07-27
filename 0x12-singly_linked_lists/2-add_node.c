e7495dfd5b0f7b7a9b7f7d084d151777fef3e2ce
Khaled-taher <khaledtaher98@gmail.com>
2023-07-27 21:04:36 +0300

    all tasks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,27 +1,22 @@
 #include "lists.h"
 
 /**
- * add_node - concatinate to strings
- * @head: pointer to first element
- * @str: string to be writen in the element
- * Return: address to new element
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to the head of the list
+ * @str: string to be duplicated
+ *
+ * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	int i;
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-
-	for (i = 0; str[i] != '\0';  i++)
-		i++;
-
 	new->str = strdup(str);
-	new->len = i;
+	new->len = strlen(str);
 	new->next = *head;
 	*head = new;
-
 	return (new);
 }
