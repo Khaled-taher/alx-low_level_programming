1c3a93bd99ad0fb03c288aac303c41e83eaa1424
Khaled-taher <77932329+Khaled-taher@users.noreply.github.com>
2023-08-01 14:03:06 +0300

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,7 +9,7 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL, *next = NULL, *curr = *head;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (NULL);
 	
 	while (curr)
