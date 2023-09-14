e5711754809e69dbc6429ab37e4a0e73ce197d21
Khaled-taher <khaledtaher98@gmail.com>
2023-09-14 18:33:43 +0300

    all tasks

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,42 +1,34 @@
 #include "lists.h"
-
 /**
- * delete_dnodeint_at_index - concatinate to strings
- * @head: pointer to head of list
- * @index: index to add node at
- *
- * Return: sum of integer in all nodes
+ * delete_dnodeint_at_index - deletes the node at index od a DLL
+ * @head: double pointer to the head of the list
+ * @index: the node that should be delete
+ * Return: 1 if it succeeded or -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
-	size_t num_node = 0;
+	dlistint_t *aux_head = *head;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 		return (-1);
-	current = *head;
-	if (index == 0)
+	while (index > 0)
 	{
-		current->next->prev = current->prev;
-		free(current);
-		return (1);
+		if (aux_head == NULL)
+			return (-1);
+		aux_head = aux_head->next;
+		index--;
 	}
-	while (current->next)
+	if (aux_head == *head)
 	{
-		if (num_node == index)
-		{
-			current->prev->next = current->next;
-			current->next->prev = current->prev;
-			free(current);
-			return (1);
-		}
-		current = current->next;
-		num_node++;
+		*head = aux_head->next;
+		(*head)->prev = NULL;
 	}
-	if (num_node == index)
+	else
 	{
-		current->prev->next = current->next;
-		return (1);
+		aux_head->prev->next = aux_head->next;
+		if (aux_head->next)
+			aux_head->next->prev = aux_head->prev;
 	}
-	return (-1);
+	free(aux_head);
+	return (1);
 }
